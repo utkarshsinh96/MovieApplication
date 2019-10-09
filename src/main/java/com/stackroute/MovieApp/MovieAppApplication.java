eba7b3213184631b423b56e0b8c8b96ab3e3b03c
utkarshsinh96 <utkarshsinh96@gmail.com>
2019-10-09 12:10:13 +0530

    swagger

--- src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
+++ src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.MovieApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/stackroute/MovieApp/service/MovieServiceImpl.java
+++ src/main/java/com/stackroute/MovieApp/service/MovieServiceImpl.java
@@ -20,7 +20,14 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie saveNewMovie(Movie movie)  {
+//    if(movieRepository.existsById(movie.getId())){
+//      throw new Exception("Movie already exits");
+//    }
         Movie savedMovie = movieRepository.save(movie);
+//    if(savedMovie == null)
+//    {
+//      throw new Exception("Movie already exists.");
+//    }
         return savedMovie;
     }
 
@@ -32,12 +39,18 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Optional<Movie> getById(int id) {
         Optional<Movie> movieId = movieRepository.findById(id);
+//    if (movieId.isEmpty()){
+//      throw new Exception("Movie not found");
+//    }
         return movieId;
     }
 
     @Override
     public boolean deleteById(int id){
         Optional<Movie> movieId = movieRepository.findById(id);
+//    if (movieId.isEmpty()){
+//      throw new Exception("Movie not found");
+//    }
         movieRepository.deleteById(id);
         return true;
 
@@ -46,6 +59,9 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie updateById(Movie movie, int id) {
         Optional<Movie> userOptional = movieRepository.findById(id);
+//    if(userOptional.isEmpty()){
+//      throw new Exception("Track not found!");
+//    }
         movie.setId(id);
         movieRepository.save(movie);
         return movie;
